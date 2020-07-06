99908edaf98c09c7e782a1855fb73d39363b3377
nvan23 <dtnvan230898@gmail.com>
2020-07-06 23:51:57 +0700

    Feature: add view for list all posts

---
+++ .vuepress/components/all-posts.vue
@@ -0,0 +1,8 @@
+// .vuepress/components/all-posts.vue
+<template>
+  <div>
+    <p v-for="post in $site.pages.filter(p => p.path.includes('/posts'))">
+      <router-link :to="post.path">{{ post.title }}</router-link>
+    </p>
+  </div>
+</template>
