850a7102168c744ef56e893a3b98e069d4b86125
nvan23 <dtnvan230898@gmail.com>
2020-07-06 22:03:58 +0700

    Feature: Adđ README.md file to replace 404 pages

---
+++ .vuepress/config.js
@@ -0,0 +1,25 @@
+module.exports  =  {
+  title: 'HeeYee Gaming',
+  themeConfig: {
+    sidebar: 'auto', // Luôn hiển thị sidebar trong bài viết
+    // Thiết lập các liên kế trên navbar
+    nav: [
+      { text: 'Trang Chủ', link: '/' },
+      { text: 'Giới Thiệu', link: '/about/' },
+      { text: 'Ehkoo', link: 'https://ehkoo.com' },
+    ],
+  },
+  description: 'Mô tả về blog',
+  locales:  {
+    '/vi':  {
+      lang:  'vi-VN',
+      title:  'Tên blog tiếng việt',
+      description:  'Mô tả tiếng việt'
+    },
+    '/en':  {
+      lang:  'en-VN',
+      title:  'Tên blog tiếng anh',
+      description:  'Mô tả tiếng anh'
+    }
+  },
+}
